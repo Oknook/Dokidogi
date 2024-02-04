d5608a1d2c7c324dada8688b34a5f43f53b5c5a2
주준형 <j40605@hanyang.ac.kr>
2024-02-04 17:58:39 +0900

    [Feat:Front]: 'PetProfile Organization'

---
+++ Front/doggydoggy_front/src/components/PetProfile.vue
@@ -0,0 +1,21 @@
+<script>
+import Header from '@/components/common/Header.vue'
+export default {
+    components : {
+        Header: Header
+    }
+}
+</script>
+
+<template>
+<Header></Header>
+
+
+
+</template>
+
+
+
+<style>
+
+</style>
