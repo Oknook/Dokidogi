24ca6299fd7d1622fd69804040a452e2971f2199
oo5890 <oo5890@naver.com>
2024-02-12 16:29:12 +0900

    [Feat:Front]: " server test"

--- Front/doggydoggy_front/vite.config.js
+++ Front/doggydoggy_front/vite.config.js
@@ -1,7 +1,6 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +9,19 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    proxy: {
+      // '/api' is the path to proxy.
+      // For example, if you make a request to '/api/users' in a Vue.js application,
+      // This request is forwarded to 'http://localhost:8080/api/users'.
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true, // Corrected: semicolon to comma
+        secure: false,
+      },
+    },
+  },
+});
