c2240c9543c58aecbe04cead97d331eef28bda3d
주준형 <j40605@hanyang.ac.kr>
2024-02-13 22:33:24 +0900

    [Front:Feat] 'footer added'

--- Front/doggydoggy_front/src/App.vue
+++ Front/doggydoggy_front/src/App.vue
@@ -2,17 +2,20 @@
   <div class="App">
     <Header></Header>
     <router-view />
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
 import HomeView from './views/HomeView.vue';
 import Header from '@/components/common/Header.vue';
+import Footer from './components/common/Footer.vue';
 
 export default {
   components: {
     'HomeView': HomeView,
     'Header': Header,
+    'Footer': Footer,
   },
   methods: {},
 };
