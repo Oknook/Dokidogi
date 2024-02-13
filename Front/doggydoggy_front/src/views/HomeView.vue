37332be7541a7c3fe4747be68015f4c728b19be1
주준형 <j40605@hanyang.ac.kr>
2024-02-13 09:40:29 +0900

    [Feat:Front] 'git conflict?'

--- Front/doggydoggy_front/src/views/HomeView.vue
+++ Front/doggydoggy_front/src/views/HomeView.vue
@@ -4,20 +4,16 @@ import { useUserStore } from '@/stores/userStore';
 import axios from 'axios';
 import { onMounted } from 'vue';
 const userStore = useUserStore();
-
-
 </script>
 
 <template>
-  <div class=".home">
-    <button @click="press">button</button>
-  </div>
+  
+
+
+
+
 </template>
 
 <style scoped>
-.home {
-  height: 100%;
-  background-color: whitesmoke;
-  text-align: center;
-}
+
 </style>
